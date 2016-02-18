3071144b423cc090710b47f39500564c4e5102f0
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-18 14:30:14 +0530

    Added analytics profile

---
+++ src/DotNetAnalytics.Google/Profiles/AnalyticsProfile.cs
@@ -0,0 +1,44 @@
+﻿namespace DotNetAnalytics.Google.Profiles
+{
+    /// <summary>
+    ///     Encapsulates profile information used to connect to the Google Analytics API.
+    /// </summary>
+    public class AnalyticsProfile : IAnalyticsProfile
+    {
+        /// <summary>
+        ///     Instantiates an <seealso cref="AnalyticsProfile" /> with the given view id, service account email, key file, and
+        ///     application name.
+        /// </summary>
+        /// <param name="viewId">The numeric id number of the View to connect to.</param>
+        /// <param name="serviceAccountEmail">The email address of the service account connected to the View.</param>
+        /// <param name="keyFilePath">The path to the credential key file as a .p12 file.</param>
+        /// <param name="applicationName">The name of the application requesting analytics, for reporting purposes.</param>
+        public AnalyticsProfile(uint viewId, string serviceAccountEmail, string keyFilePath, string applicationName)
+        {
+            ViewId = viewId;
+            ServiceAccountEmail = serviceAccountEmail;
+            KeyFilePath = keyFilePath;
+            ApplicationName = applicationName;
+        }
+
+        /// <summary>
+        ///     The numeric id number of the View to connect to.
+        /// </summary>
+        public uint ViewId { get; }
+
+        /// <summary>
+        ///     The email address of the service account connected to the View.
+        /// </summary>
+        public string ServiceAccountEmail { get; }
+
+        /// <summary>
+        ///     The path to the credential key file as a .p12 file.
+        /// </summary>
+        public string KeyFilePath { get; }
+
+        /// <summary>
+        ///     The name of the application requesting analytics, for reporting purposes.
+        /// </summary>
+        public string ApplicationName { get; }
+    }
+}

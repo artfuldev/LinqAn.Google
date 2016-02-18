54a5a38c3cc5b244905bece530a250ecbf5f971d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-18 16:45:07 +0530

    Removed unnecessary project and added Sample

---
+++ samples/DotNetAnalytics.Google.Sample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetAnalytics.Google.Dimensions;
+using DotNetAnalytics.Google.Metrics;
+using DotNetAnalytics.Google.Profiles;
+using Newtonsoft.Json;
+
+namespace DotNetAnalytics.Google.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appSettingsReader = new AppSettingsReader();
+            var viewId = (uint)appSettingsReader.GetValue("ViewId", typeof(uint));
+            var serviceEmail = (string)appSettingsReader.GetValue("ServiceEmail", typeof(string));
+            var applicationName = (string)appSettingsReader.GetValue("ApplicationName", typeof (string));
+            var keyFilePath = Directory.GetCurrentDirectory() + @"\key.p12";
+            var profile = new AnalyticsProfile(viewId, serviceEmail, keyFilePath, applicationName);
+            var reportingClient = new ReportingClient(profile);
+            var metrics = new IMetric[]
+            {
+                new HitsMetric(),
+                new SessionsMetric(),
+                new SessionDurationMetric()
+            };
+            var dimensions = new IDimension[]
+            {
+                new SourceDimension(),
+                new MediumDimension(),
+            };
+            var records = reportingClient.GetAllRecords(DateTime.Today, metrics, dimensions);
+            foreach (var record in records)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(record));
+            }
+            Console.ReadLine();
+        }
+    }
+}

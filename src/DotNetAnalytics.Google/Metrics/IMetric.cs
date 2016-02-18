31f00c16f48185cb9917e0d5afc877a49c09155b
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-18 11:19:34 +0530

    Added Metric

--- src/DotNetAnalytics.Google/Metrics/IMetric.cs
+++ src/DotNetAnalytics.Google/Metrics/IMetric.cs
@@ -1,4 +1,6 @@
-﻿namespace DotNetAnalytics.Google.Core
+﻿using DotNetAnalytics.Google.Core;
+
+namespace DotNetAnalytics.Google.Metrics
 {
     /// <summary>
     ///     Represents a metric in Google Analytics which measures measurements of type <see cref="T" />.
@@ -6,5 +8,9 @@
     /// <typeparam name="T">The type of measure this metric measures.</typeparam>
     public interface IMetric<T> : IDescribable, ISupportSegmentation, IIndexable
     {
+        /// <summary>
+        ///     The value measured by this <seealso cref="IMetric{T}"/>.
+        /// </summary>
+        T Value { get; set; }
     }
 }
---
+++ src/DotNetAnalytics.Google/Metrics/Metric.cs
@@ -0,0 +1,19 @@
+﻿namespace DotNetAnalytics.Google.Metrics
+{
+    public abstract class Metric<T> : IMetric<T>
+    {
+        protected Metric(string name, string description, bool allowedInSegments, string id)
+        {
+            Name = name;
+            Description = description;
+            AllowedInSegments = allowedInSegments;
+            Id = id;
+        }
+
+        public string Name { get; }
+        public string Description { get; }
+        public bool AllowedInSegments { get; }
+        public string Id { get; }
+        public T Value { get; set; }
+    }
+}

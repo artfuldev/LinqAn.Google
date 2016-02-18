0b824e412dbb629d4a83dcd7285c126a9d70809d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-18 09:53:45 +0530

    Added ISupportQuerying

--- src/DotNetAnalytics.Google/Core/IMetric.cs
+++ src/DotNetAnalytics.Google/Core/IMetric.cs
@@ -4,10 +4,7 @@
     ///     Represents a metric in Google Analytics which measures measurements of type <see cref="T" />.
     /// </summary>
     /// <typeparam name="T">The type of measure this metric measures.</typeparam>
-    public interface IMetric<T> : ISupportSegmentation
+    public interface IMetric<T> : IDescribable, ISupportSegmentation, ISupportQuerying
     {
-        string Name { get; }
-        string QueryName { get; }
-
     }
 }

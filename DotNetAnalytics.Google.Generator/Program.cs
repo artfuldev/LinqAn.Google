20fc8b7714958ea141c23c100014955f3f602dea
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-18 10:49:31 +0530

    Added file writing etc

--- DotNetAnalytics.Google.Generator/Program.cs
+++ DotNetAnalytics.Google.Generator/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Humanizer;
 using Newtonsoft.Json;
 
 namespace DotNetAnalytics.Google.Generator
@@ -12,7 +13,8 @@ namespace DotNetAnalytics.Google.Generator
     {
         static void Main(string[] args)
         {
-            var jsonPath = Directory.GetCurrentDirectory() + @"\MetaData.json";
+            var currentDirectory = Directory.GetCurrentDirectory() + @"\";
+            var jsonPath = currentDirectory + "MetaData.json";
             var json = File.ReadAllText(jsonPath);
             var metaData = JsonConvert.DeserializeObject<MetaData>(json);
             var columns = metaData.Items;
@@ -20,7 +22,27 @@ namespace DotNetAnalytics.Google.Generator
                 // Remove deprecated
                 .Where(x => !x.Attributes.IsDeprecated)
                 // Remove calculated
-                .Where(x=>string.IsNullOrWhiteSpace(x.Attributes.Calculation));
+                .Where(x => string.IsNullOrWhiteSpace(x.Attributes.Calculation))
+                .ToList();
+
+            var metrics = selectedColumns.Where(x => x.Attributes.Type == "METRIC");
+            var dimensions = selectedColumns.Where(x => x.Attributes.Type == "DIMENSION");
+
+            var metricsPath = currentDirectory + "Metrics";
+            Directory.CreateDirectory(metricsPath);
+            foreach (var metric in metrics)
+            {
+                var name = metric.Attributes.UiName.Pascalize();
+                var filePath = $"{metricsPath}\\{name}.cs";
+                if (!File.Exists(filePath))
+                    File.Create(filePath);
+                var fileContent = "This is the metric file for " + name;
+                using (var tw = new StreamWriter(filePath))
+                {
+                    tw.WriteLine(fileContent);
+                    tw.Close();
+                }
+            }
         }
     }
 }

4d96a1a171158a34ce72b043569216136bbb1e6c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-18 09:47:39 +0530

    Adding core

---
+++ src/DotNetAnalytics.Google/Core/IMetric.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetAnalytics.Google.Core
+{
+    /// <summary>
+    ///     Represents a metric in Google Analytics which measures measurements of type <see cref="T" />.
+    /// </summary>
+    /// <typeparam name="T">The type of measure this metric measures.</typeparam>
+    public interface IMetric<T> : ISupportSegmentation
+    {
+        string Name { get; }
+        string QueryName { get; }
+
+    }
+}
---
+++ src/DotNetAnalytics.Google/Core/ISupportSegmentation.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetAnalytics.Google.Core
+{
+    /// <summary>
+    ///     An entity that supports segmentation in Google ANalytics.
+    /// </summary>
+    public interface ISupportSegmentation
+    {
+        /// <summary>
+        ///     Represents if this item is allowed in segments.
+        /// </summary>
+        bool AllowedInSegments { get; }
+    }
+}

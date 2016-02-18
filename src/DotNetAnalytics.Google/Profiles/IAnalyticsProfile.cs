17004194fc74fd51d7279d74175f8630c4f336b6
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-18 14:27:27 +0530

    Added IAnalyticsProfile

---
+++ src/DotNetAnalytics.Google/Profiles/IAnalyticsProfile.cs
@@ -0,0 +1,28 @@
+﻿namespace DotNetAnalytics.Google.Profiles
+{
+    /// <summary>
+    ///     Encapsulates profile information used to connect to the Google Analytics API.
+    /// </summary>
+    public interface IAnalyticsProfile
+    {
+        /// <summary>
+        ///     The numeric id number of the View to connect to.
+        /// </summary>
+        uint ViewId { get; }
+
+        /// <summary>
+        ///     The email address of the service account connected to the View.
+        /// </summary>
+        string ServiceAccountEmail { get; }
+
+        /// <summary>
+        ///     The path to the credential key file as a .p12 file.
+        /// </summary>
+        string KeyFilePath { get; }
+
+        /// <summary>
+        ///     The name of the application requesting analytics, for reporting purposes.
+        /// </summary>
+        string ApplicationName { get; }
+    }
+}

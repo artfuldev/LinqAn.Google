01486a5715b9d308f3157049e1a61b7a61fed91e
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-19 23:38:15 +0530

    Changed into AsEnumerable

--- src/DotNetAnalytics.Google.Linq/Queryables/RecordsQueryableExtensions.cs
+++ src/DotNetAnalytics.Google.Linq/Queryables/RecordsQueryableExtensions.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using DotNetAnalytics.Google.Records;
 
 namespace DotNetAnalytics.Google.Linq.Queryables
 {
     public static class RecordsQueryableExtensions
     {
-        public static async Task<List<T>> ToListAsync<T>(this IQueryable<T> source) where T : IRecord
+        public static IEnumerable<IRecord> AsEnumerable(this IQueryable<IRecord> source)
         {
-            return (await source.Provider.Execute<Task<IEnumerable<T>>>(source.Expression)).ToList();
+            return source.Provider.Execute<IEnumerable<IRecord>>(source.Expression);
         }
     }
 }

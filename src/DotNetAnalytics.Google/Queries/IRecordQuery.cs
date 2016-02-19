62ea098f40c81812fe455b60470c550fa542f52c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-19 22:50:17 +0530

    Adding IRecordQuery

---
+++ src/DotNetAnalytics.Google/Queries/IRecordQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using DotNetAnalytics.Google.Dimensions;
+using DotNetAnalytics.Google.Metrics;
+
+namespace DotNetAnalytics.Google.Queries
+{
+    public interface IRecordQuery
+    {
+        uint ViewId { get; }
+        DateTime StartDate { get; }
+        DateTime EndDate { get; }
+        IEnumerable<IMetric> Metrics { get; }
+        IEnumerable<IDimension> Dimensions { get; }
+        uint StartIndex { get; }
+        uint RecordsCount { get; }
+    }
+}
---
+++ src/DotNetAnalytics.Google/Queries/RecordQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DotNetAnalytics.Google.Dimensions;
+using DotNetAnalytics.Google.Metrics;
+
+namespace DotNetAnalytics.Google.Queries
+{
+    public class RecordQuery : IRecordQuery
+    {
+        public const uint MaxRecordsPerQuery = 10000U;
+
+        public RecordQuery(uint viewId, DateTime startDate, DateTime endDate, IEnumerable<IMetric> metrics,
+            IEnumerable<IDimension> dimensions = null, uint startIndex = 1, uint recordsCount = MaxRecordsPerQuery)
+        {
+            ViewId = viewId;
+            StartDate = startDate;
+            EndDate = endDate;
+            Metrics = metrics;
+            Dimensions = dimensions ?? Enumerable.Empty<IDimension>();
+            StartIndex = startIndex;
+            RecordsCount = recordsCount;
+        }
+
+        public uint ViewId { get; }
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+        public IEnumerable<IMetric> Metrics { get; }
+        public IEnumerable<IDimension> Dimensions { get; }
+        public uint StartIndex { get; }
+        public uint RecordsCount { get; }
+    }
+}

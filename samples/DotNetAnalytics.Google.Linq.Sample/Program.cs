d5652a994e198bc9bff86f0ecd4adb2a2219fee5
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-19 23:11:09 +0530

    Added Linq sample (not working right now)

---
+++ samples/DotNetAnalytics.Google.Linq.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetAnalytics.Google.Linq.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

2c473c0aa6f40a14f75e67ddac46639213df4aaf
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-20 14:42:31 +0530

    Simplified file generation by refactoring

--- tools/LinqAn.Google.Generator/Program.cs
+++ tools/LinqAn.Google.Generator/Program.cs
@@ -10,6 +10,28 @@ namespace LinqAn.Google.Generator
     {
         private static readonly IFileContentGenerator MetricFileContentGenerator = new MetricFileContentGenerator();
         private static readonly IFileContentGenerator DimensionFileContentGenerator = new DimensionFileContentGenerator();
+
+        private static void GenerateFiles(string path, IEnumerable<Column> columns, IFileContentGenerator generator,
+            params string[] exlusions)
+        {
+            Directory.CreateDirectory(path);
+            var allFiles = new DirectoryInfo(path).GetFiles();
+            exlusions = exlusions ?? new string[0];
+            var files = allFiles.Where(x => !exlusions.Contains(x.Name));
+            foreach (var file in files)
+                file.Delete();
+            foreach (var column in columns)
+            {
+                var name = generator.GetFileName(column);
+                var filePath = $"{path}\\{name}.cs";
+                var fileContent = generator.GenerateFileContent(column);
+                using (var tw = new StreamWriter(File.OpenWrite(filePath)))
+                {
+                    tw.WriteLine(fileContent);
+                }
+            }
+
+        }
         static void Main(string[] args)
         {
             var currentDirectory = Directory.GetCurrentDirectory() + @"\";
@@ -37,37 +59,9 @@ namespace LinqAn.Google.Generator
                 dimension.Attributes.UiName = dimension.Id.Replace("ga:", "").Pascalize().Humanize().Titleize();
 
             var metricsPath = currentDirectory.Replace(@"\tools\LinqAn.Google.Generator", @"\src\LinqAn.Google\Metrics");
-            Directory.CreateDirectory(metricsPath);
-            var files = new DirectoryInfo(metricsPath).GetFiles();
-            var metricFiles = files.Where(x => x.Name != "IMetric.cs" && x.Name != "Metric.cs");
-            foreach (var file in metricFiles)
-                file.Delete();
-            foreach (var metric in metricsList)
-            {
-                var name = MetricFileContentGenerator.GetFileName(metric);
-                var filePath = $"{metricsPath}\\{name}.cs";
-                var fileContent = MetricFileContentGenerator.GenerateFileContent(metric);
-                using (var tw = new StreamWriter(File.OpenWrite(filePath)))
-                {
-                    tw.WriteLine(fileContent);
-                }
-            }
+            GenerateFiles(metricsPath, metricsList, MetricFileContentGenerator, "IMetric.cs", "Metric.cs");
             var dimensionsPath = currentDirectory.Replace(@"\tools\LinqAn.Google.Generator", @"\src\LinqAn.Google\Dimensions");
-            Directory.CreateDirectory(dimensionsPath);
-            var existingDimensionFiles = new DirectoryInfo(dimensionsPath).GetFiles();
-            var dimensionFiles = existingDimensionFiles.Where(x => x.Name != "IDimension.cs" && x.Name != "Dimension.cs");
-            foreach (var file in dimensionFiles)
-                file.Delete();
-            foreach (var dimension in dimensionList)
-            {
-                var name = DimensionFileContentGenerator.GetFileName(dimension);
-                var filePath = $"{dimensionsPath}\\{name}.cs";
-                var fileContent = DimensionFileContentGenerator.GenerateFileContent(dimension);
-                using (var tw = new StreamWriter(File.OpenWrite(filePath)))
-                {
-                    tw.WriteLine(fileContent);
-                }
-            }
+            GenerateFiles(dimensionsPath, dimensionList, DimensionFileContentGenerator, "IDimension.cs", "Dimension.cs");
         }
     }
 }

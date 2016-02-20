4da063d208ab0e106b7dff8d9d3f2aa2ef0acfb3
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-20 14:31:09 +0530

    Readded operators

--- src/LinqAn.Google/Dimensions/Dimension.cs
+++ src/LinqAn.Google/Dimensions/Dimension.cs
@@ -1,4 +1,6 @@
-﻿namespace LinqAn.Google.Dimensions
+﻿using System.Text.RegularExpressions;
+
+namespace LinqAn.Google.Dimensions
 {
 #pragma warning disable 660,661
     public abstract class Dimension<T> : IDimension<T>
@@ -17,5 +19,28 @@
         public bool AllowedInSegments { get; }
         public string Id { get; }
         public T Value { get; set; }
+
+        public static bool operator ==(Dimension<T> dimension, T value)
+        {
+            if (ReferenceEquals(dimension, value))
+                return true;
+            return !ReferenceEquals(dimension, null) && Equals(dimension.Value, value);
+        }
+
+        public static bool operator !=(Dimension<T> dimension, T value)
+        {
+            return !(dimension == value);
+        }
+
+        public static bool operator ==(Dimension<T> dimension, Regex value)
+        {
+            return !ReferenceEquals(dimension, null) && !ReferenceEquals(value, null) &&
+                   value.IsMatch(dimension.Value.ToString());
+        }
+
+        public static bool operator !=(Dimension<T> dimension, Regex value)
+        {
+            return !(dimension == value);
+        }
     }
 }
--- src/LinqAn.Google/Metrics/Metric.cs
+++ src/LinqAn.Google/Metrics/Metric.cs
@@ -1,4 +1,7 @@
-﻿namespace LinqAn.Google.Metrics
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LinqAn.Google.Metrics
 {
 #pragma warning disable 660,661
     public abstract class Metric<T> : IMetric<T>
@@ -17,5 +20,61 @@
         public bool AllowedInSegments { get; }
         public string Id { get; }
         public T Value { get; set; }
+
+        public static bool operator ==(Metric<T> metric, T value)
+        {
+            if (ReferenceEquals(metric, value))
+                return true;
+            return !ReferenceEquals(metric, null) && Equals(metric.Value, value);
+        }
+
+        public static bool operator !=(Metric<T> metric, T value)
+        {
+            return !(metric == value);
+        }
+
+        public static bool operator ==(Metric<T> metric, Regex value)
+        {
+            return !ReferenceEquals(metric, null) && !ReferenceEquals(value, null) &&
+                   value.IsMatch(metric.Value.ToString());
+        }
+
+        public static bool operator !=(Metric<T> metric, Regex value)
+        {
+            return !(metric == value);
+        }
+
+        public static bool operator <(Metric<T> metric, T value)
+        {
+            if (ReferenceEquals(metric, null))
+                return false;
+            if (typeof(T) == typeof(string))
+                throw new InvalidOperationException("Cannot compare two strings in this fashion");
+            return NumericallyLessThan(metric.Value, value);
+        }
+
+        public static bool operator >(Metric<T> metric, T value)
+        {
+            return !(metric < value) && metric != value;
+        }
+
+        public static bool operator <=(Metric<T> metric, T value)
+        {
+            return metric < value || metric == value;
+        }
+
+        public static bool operator >=(Metric<T> metric, T value)
+        {
+            return metric > value || metric == value;
+        }
+
+        private static bool NumericallyLessThan(object left, object right)
+        {
+            if (left is TimeSpan)
+                return (TimeSpan)left < (TimeSpan)right;
+            if (left is int || left is uint || left is decimal || left is double || left is float)
+                return Convert.ToDecimal(left) < Convert.ToDecimal(right);
+            return false;
+        }
     }
 }

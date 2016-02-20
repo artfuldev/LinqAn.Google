afa5e7eeb4b3e0bf47b06770eeaa73a44dd5e182
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-20 11:57:51 +0530

    Updated settings, changed project type

--- samples/LinqAn.Google.Sample/Program.cs
+++ samples/LinqAn.Google.Sample/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace LinqAn.Google.Sample
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder().AddJsonFile("gasettings.json");
+            var config = builder.Build();
+            var viewId = Convert.ToUInt32(config["profile:view_id"]);
+            var serviceEmail = config["profile:service_account_email"];
+            var applicationName = config["profile:application_name"];
+            var keyFilePath = Directory.GetCurrentDirectory() + "\\" + config["profile:key_file_name"];
+            var profile = new AnalyticsProfile(serviceEmail, keyFilePath, applicationName);
+            var reportingClient = new ReportingClient(profile);
+            var repository = new Repository(reportingClient);
+            var query = repository
+                // View Id
+                .Query(x => x.ViewId == viewId)
+                // Include Dimensions
+                .Include(x => new SourceDimension())
+                .Include(x => new MediumDimension())
+                // Include Metrics
+                .Include(x => new HitsMetric())
+                .Include(x => new SessionsMetric())
+                .Include(x => new SessionDurationMetric());
+
+            var records = query.Select().ToList();
+            foreach (var record in records)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(record));
+            }
+            Console.ReadLine();
         }
     }
 }
--- samples/LinqAn.Google.Sample/Properties/AssemblyInfo.cs
+++ samples/LinqAn.Google.Sample/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LinqAn.Google.Sample")]
@@ -14,23 +14,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6d862d40-faf3-4e50-8046-d4587fc2ec0e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("86e03db2-6cd9-463e-b075-3498393c64af")]

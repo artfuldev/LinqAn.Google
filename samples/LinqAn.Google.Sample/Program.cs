79c588337d48d3061b5c7b3dc3e711068266dd48
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-21 22:40:06 +0530

    Added working sample

--- samples/LinqAn.Google.Sample/Program.cs
+++ samples/LinqAn.Google.Sample/Program.cs
@@ -5,6 +5,7 @@ using LinqAn.Google.Profiles;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System.Linq;
+using LinqAn.Google.Extensions;
 using LinqAn.Google.Linq.Queryables;
 
 namespace LinqAn.Google.Sample
@@ -24,9 +25,20 @@ namespace LinqAn.Google.Sample
             var recordsDataSet = new RecordsDataSet(reportingClient);
             var query = recordsDataSet
                 // View Id
-                .Where(x => x.ViewId == viewId);
+                .Where(x => x.ViewId == viewId)
+                // Start Date, End Date
+                .Where(x => x.RecordDate == DateTime.Today)
+                // Include Dimensions
+                .Include(x => x.Source)
+                .Include(x => x.Medium)
+                // Include Metrics
+                .Include(x => x.Hits)
+                .Include(x => x.Sessions)
+                .Include(x => x.SessionDuration)
+                // Take only 2 records
+                .Take(2);
 
-            var records = query.ToList();
+            var records = query.ToList().Select(x => x.ToRecord());
             foreach (var record in records)
             {
                 Console.WriteLine(JsonConvert.SerializeObject(record));

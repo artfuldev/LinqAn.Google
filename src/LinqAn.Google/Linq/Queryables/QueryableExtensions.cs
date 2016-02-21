cb4c52e4db79d9ffb3cf94aeb50aa6493247c86e
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-21 21:50:52 +0530

    Changed type parameters.

--- src/LinqAn.Google/Linq/Queryables/QueryableExtensions.cs
+++ src/LinqAn.Google/Linq/Queryables/QueryableExtensions.cs
@@ -9,9 +9,11 @@ namespace LinqAn.Google.Linq.Queryables
 {
     public static class QueryableExtensions
     {
-        public static IQueryable<IQueryableRecord> Include<T,TProperty>(this IQueryable<IQueryableRecord> source, Expression<Func<T,TProperty>> includeExpression)
+        public static IQueryable<IQueryableRecord> Include<T>(this IQueryable<IQueryableRecord> source,
+            Expression<Func<IQueryableRecord, T>> includeExpression)
         {
-            var method = source.GetType().GetRuntimeMethod("Include", new[] { typeof(Expression<Func<T,TProperty>>)});
+            var method = source.GetType()
+                .GetRuntimeMethod("Include", new[] {typeof (Expression<Func<IQueryableRecord, T>>)});
             method?.Invoke(source, new object[] {includeExpression});
             return source;
         }

db8702d0798ab1241c1206230fd58e87bc105f0d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-21 22:43:03 +0530

    Removed unused extensions.

--- src/LinqAn.Google/Linq/Queryables/RecordsQueryableExtensions.cs
+++ src/LinqAn.Google/Linq/Queryables/RecordsQueryableExtensions.cs
@@ -1,25 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
-using System.Threading.Tasks;
 using LinqAn.Google.Records;
 
 namespace LinqAn.Google.Linq.Queryables
 {
     public static class RecordsQueryableExtensions
     {
-        public static IEnumerable<IQueryableRecord> AsEnumerable(this IQueryable<IQueryableRecord> source)
-        {
-            return source.Provider.Execute<IEnumerable<IQueryableRecord>>(source.Expression);
-        }
-
-        public static Task<List<IQueryableRecord>> ToListAsync(this IQueryable<IQueryableRecord> source)
-        {
-            return Task.FromResult(source.AsEnumerable().ToList());
-        }
-
         public static IQueryable<IQueryableRecord> Include<T>(this IQueryable<IQueryableRecord> source,
             Expression<Func<IQueryableRecord, T>> includeExpression)
         {

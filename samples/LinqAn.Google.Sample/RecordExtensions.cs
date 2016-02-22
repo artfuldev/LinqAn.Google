3b552f143f938aa39aa67c72e6be8ff37cd47273
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-22 12:40:04 +0530

    Updated sample

--- samples/LinqAn.Google.Sample/RecordExtensions.cs
+++ samples/LinqAn.Google.Sample/RecordExtensions.cs
@@ -1,31 +1,14 @@
-﻿using System.Reflection;
-using System.Text;
-using LinqAn.Google.Dimensions;
-using LinqAn.Google.Metrics;
+﻿using System.Linq;
 using LinqAn.Google.Records;
 
 namespace LinqAn.Google.Sample
 {
     public static class RecordExtensions
     {
-        public static string ToStringRepresentation(this IDimension dimension)
-        {
-            return $"{dimension.Name} : {dimension.GetType().GetRuntimeProperty("Value").GetValue(dimension)}";
-        }
-
-        public static string ToStringRepresentation(this IMetric metric)
-        {
-            return $"{metric.Name} : {metric.GetType().GetRuntimeProperty("Value").GetValue(metric)}";
-        }
-
         public static string ToStringRepresentation(this IRecord record)
         {
-            var representaion = new StringBuilder();
-            foreach (var dimension in record.Dimensions)
-                representaion.AppendLine(dimension.ToStringRepresentation());
-            foreach (var metric in record.Metrics)
-                representaion.AppendLine(metric.ToStringRepresentation());
-            return representaion.ToString();
+            var representaion = record.Dimensions.Aggregate("", (current, dimension) => current + dimension + "\n");
+            return record.Metrics.Aggregate(representaion, (current, metric) => current + metric + "\n");
         }
     }
 }

69afdde8a6f65117554c96ec41360c4cae1194e4
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-23 11:53:38 +0530

    Fixed Names.

--- src/LinqAn.Google/Metrics/OnedayUsers.cs
+++ src/LinqAn.Google/Metrics/OnedayUsers.cs
@@ -6,12 +6,12 @@ namespace LinqAn.Google.Metrics
 	/// 	Total number of 1-day active users for each day in the requested time period. At least one of ga:nthDay, ga:date, or ga:day must be specified as a dimension in order to query this metric. For the given date the returned value will be the total unique users for the 1 day period ending on the given date.
 	/// </summary>
 	[Description("Total number of 1-day active users for each day in the requested time period. At least one of ga:nthDay, ga:date, or ga:day must be specified as a dimension in order to query this metric. For the given date the returned value will be the total unique users for the 1 day period ending on the given date.")]
-	public class OnedayUsers: Metric<int>
+	public class OneDayUsers: Metric<int>
 	{
 		/// <summary>
-		/// 	Instantiates a <seealso cref="OnedayUsers" />.
+		/// 	Instantiates a <seealso cref="OneDayUsers" />.
 		/// </summary>
-		public OnedayUsers(): base("1 Day Active Users",false,"ga:1dayUsers")
+		public OneDayUsers(): base("1 Day Active Users",false,"ga:1dayUsers")
 		{
 			
 		}

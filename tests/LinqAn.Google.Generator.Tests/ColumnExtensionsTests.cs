593fbdc6b3da011c7e588257d0c2c9f53709e72a
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-23 11:47:03 +0530

    Changed GetClassName.

---
+++ tests/LinqAn.Google.Generator.Tests/ColumnExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using LinqAn.Google.Generator.Core;
+using Xunit;
+
+namespace LinqAn.Google.Generator.Tests
+{
+    public class ColumnExtensionsTests
+    {
+        public ColumnExtensionsTests()
+        {
+        }
+
+        [Theory]
+        [InlineData("ga:21dayUsers","TwentyOneDayUsers")]
+        public void GetClassNameWorksAsExpected(string input, string expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = input.GetClassName();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}

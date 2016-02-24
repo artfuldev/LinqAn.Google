366190173899494c4b4bb933330b9db78699a19c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-24 21:14:50 +0530

    Added interface

---
+++ src/LinqAn.Google/Linq/Core/IAsyncQueryProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace LinqAn.Google.Linq.Core
+{
+    public interface IAsyncQueryProvider : IQueryProvider
+    {
+        Task<object> ExecuteAsync(Expression query);
+        Task<TResult> ExecuteAsync<TResult>(Expression query);
+    }
+}

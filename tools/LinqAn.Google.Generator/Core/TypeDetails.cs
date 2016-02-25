f21fcf920fe7f4859622c987cc6c39db975515e6
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-02-25 08:53:21 +0530

    Added TypeDetails

---
+++ tools/LinqAn.Google.Generator/Core/TypeDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace LinqAn.Google.Generator.Core
+{
+    public class TypeDetails : ITypeDetails
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool AllowedInSegments { get; set; }
+        public string TypeName { get; set; } 
+    }
+}
